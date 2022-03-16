92ef9ac11215c2d0d7473b78296a502835501e1f
jusupm <98189162+jusupm@users.noreply.github.com>
2022-03-16 10:07:17 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,22 +18,7 @@ PINK = (147,20,255)
 points_list =[(200, 300), (150, 150), (400, 200)]
  
 def colorBackgroundText(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3):
-    """
-    Draws text with background, with  control transparency
-    @param img:(mat) which you want to draw text
-    @param text: (string) text you want draw
-    @param font: fonts face, like FONT_HERSHEY_COMPLEX, FONT_HERSHEY_PLAIN etc.
-    @param fontScale: (double) the size of text, how big it should be.
-    @param textPos: tuple(x,y) position where you want to draw text
-    @param textThickness:(int) fonts weight, how bold it should be
-    @param textPos: tuple(x,y) position where you want to draw text
-    @param textThickness:(int) fonts weight, how bold it should be.
-    @param textColor: tuple(BGR), values -->0 to 255 each
-    @param bgColor: tuple(BGR), values -->0 to 255 each
-    @param pad_x: int(pixels)  padding of in x direction
-    @param pad_y: int(pixels) 1 to 1.0 (), controls transparency of  text background 
-    @return: img(mat) with draw with background
-    """
+   
     (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
     x, y = textPos
     cv.rectangle(img, (x-pad_x, y+ pad_y), (x+t_w+pad_x, y-t_h-pad_y), bgColor,-1) # draw rectangle 
@@ -42,22 +27,7 @@ def colorBackgroundText(img, text, font, fontScale, textPos, textThickness=1,tex
     return img
 
 def textWithBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3, bgOpacity=0.5):
-    """
-    Draws text with background, with  control transparency
-    @param img:(mat) which you want to draw text
-    @param text: (string) text you want draw
-    @param font: fonts face, like FONT_HERSHEY_COMPLEX, FONT_HERSHEY_PLAIN etc.
-    @param fontScale: (double) the size of text, how big it should be.
-    @param textPos: tuple(x,y) position where you want to draw text
-    @param textThickness:(int) fonts weight, how bold it should be
-    @param textPos: tuple(x,y) position where you want to draw text
-    @param textThickness:(int) fonts weight, how bold it should be.
-    @param textColor: tuple(BGR), values -->0 to 255 each
-    @param bgColor: tuple(BGR), values -->0 to 255 each
-    @param pad_x: int(pixels)  padding of in x direction
-    @param pad_y: int(pixels) 1 to 1.0 (), controls transparency of  text background 
-    @return: img(mat) with draw with background
-    """
+   
     (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
     x, y = textPos
     overlay = img.copy() # coping the image
